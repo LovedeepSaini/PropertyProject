a2e45dcfd0982f809dbd1591925afff53f76a884
Maxwell Bowie <Maxwell.Bowie@embark.co.uk>
2024-01-24 15:11:03 +0000

    added image to properties

--- src/components/PropertySearch.js
+++ src/components/PropertySearch.js
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPoundSign, faSearch, faBed, faBath , faTree, faMapMarkerAlt, faCheck} from "@fortawesome/free-solid-svg-icons";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import image from './image.jpg';
 
 function PropertySearch(props) {
   const [show, setShow] = useState(false);
@@ -127,6 +128,7 @@ const handleBooking = () => {
       />
 
       <div className="container-fluid">
+      
         {/* Bootstrap modal to edit property */}
 
         <Modal show={show} onHide={handleClose}>
@@ -134,6 +136,7 @@ const handleBooking = () => {
             <Modal.Title>Property</Modal.Title>
           </Modal.Header>
           <Modal.Body>
+          
             <br />
             <Form>
               <Form.Group className="mb-3" controlId="price1">
@@ -262,6 +265,7 @@ const handleBooking = () => {
         {result.map((property) => (
           <Col key={property.id} className="pb-4" sm={6}>
             <Card className="">
+            <Card.Img variant="top" src={image} />
               <Card.Body className="div1">
                 <Card.Title className="title">
               
