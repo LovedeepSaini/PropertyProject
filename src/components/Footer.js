a69ae33eea4371a9f356de31f20fb282f8a2dd8a
Toby Keech <Toby.Keech@embark.co.uk>
2024-01-24 09:21:47 +0000

    added fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,74 +1,83 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { NavLink } from "react-router-dom";
-import "./Navbar.css";
-import {  HamburgetMenuClose, HamburgetMenuOpen } from "./Icons";
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
-import { faUsers } from '@fortawesome/free-solid-svg-icons';
-import { faBuilding } from '@fortawesome/free-solid-svg-icons';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom";
+import {faPhone, faAt, faMapLocationDot} from "@fortawesome/free-solid-svg-icons"
+import{faInstagram, faFacebook, faLinkedin , faTwitter} from "@fortawesome/free-brands-svg-icons"
+const Footer = () => {
+    return ( 
+        <>
+<footer class="text-center text-lg-start bg-body-info text-muted">
+<section class="d-flex justify-content-between p-4 border-bottom">
+  <div class="d-flex align-items-center">
+    <span class="me-3">Connect with us on socials:</span>
+
+    <a href="" class="me-4 text-reset fa-lg">
+      <FontAwesomeIcon icon={faInstagram} style={{ color: "#bb06e0" }} />
+    </a>
+    <a href="" class="me-4 text-reset fa-lg">
+      <FontAwesomeIcon icon={faTwitter}  style={{ color: "#0792e9" }} />
+    </a>
+    <a href="" class="me-4 text-reset fa-lg">
+      <FontAwesomeIcon icon={faFacebook}  style={{ color: "#0822e2" }} />
+    </a>
+    <a href="" class="me-4 text-reset fa-lg">
+      <FontAwesomeIcon icon={faLinkedin}  style={{ color: "#0792e9" }} />
+    </a>
+  </div>
+</section>
 
 
-function Footer() {
-const [click, setClick] = useState(false);
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-2">
+          Rook
+          </h6>
+          <h6 class="text-uppercase fw-bold mb-2">
+         Residences
+          </h6>
+          <p>
+            Check out our helpful links and how we can help secure your next home.
+          </p>
+        </div>
 
-const handleClick = () => setClick(!click);
-return (
-  <>
-    <nav className="navbar navbar-expand-sm sticky-bottom">
-      <div className="container-fluid">
-        <NavLink exact to="/" className="nav-logo ">
-          <span>QA Properties</span>
-          {/* <i className="fas fa-code"></i> */}
-          
-        </NavLink>
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+          <Link to="/property" className="text-reset">
+              Properties
+          </Link>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">About Us</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
 
-        <ul className={click ? "nav-menu " : "nav-menu"}>
-        <li >
-            <NavLink
-              exact
-              to="/Home"
-              
-              className="nav-links"
-              onClick={handleClick}
-            >
-               <FontAwesomeIcon icon={faHome} />
-            </NavLink>
-          </li>
-
-          <li>
-            About Us
-           &nbsp;
-          </li>
-          <li>
-            Contact
-            &nbsp;
-          </li>
-
-          <li>
-            T&C
-            &nbsp;
-          </li>
-        
-        
-        </ul>
-        <div className="nav-icon" onClick={handleClick}>
-          {/* <i className={click ? "fas fa-times" : "fas fa-bars"}></i> */}
-
-          {click ? (
-            <span className="icon">
-              <HamburgetMenuOpen />{" "}
-            </span>
-          ) : (
-            <span className="icon">
-              <HamburgetMenuClose />
-            </span>
-          )}
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">Contact Us</h6>
+          <p class="fasme-3"> <FontAwesomeIcon icon={faMapLocationDot}/> &nbsp;186 Braveheart Lane, Perth, PH8 7LF</p>
+          <p class="fasme-3"> <FontAwesomeIcon icon={faAt}/>&nbsp;
+          RookResidences@chess.com
+          </p>
+          <p class="fasme-3"> <FontAwesomeIcon icon={faPhone}/>&nbsp;01652 864579</p>
         </div>
       </div>
-    </nav>
-  </>
-);
-}
+    </div>
+  </section>
 
+  <div class="text-center p-4">
+     2023 Copyright:&nbsp;
+    <a class="text-reset fw-bold">RookResidences.com</a>
+  </div>
+</footer>
+
+        </>
+     );
+}
+ 
 export default Footer;
